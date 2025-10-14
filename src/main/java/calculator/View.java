b3529022463b1ae1d42fefca321f757e1fc7d0c4
sotogito <sotogitoarchive@gmail.com>
2025-10-14 23:59:45 +0900

    feat : 문자열 입력 view 구현

---
+++ src/main/java/calculator/View.java
@@ -0,0 +1,19 @@
+package calculator;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class View {
+    private final static String READ_STRING_EXPRESSION = "덧셈할 문자열을 입력해 주세요.\n";
+    private final static String WRITE_RESULT_FORMAT = "결과 : %d\n";
+
+    public static String readStringExpression() {
+        try {
+            System.out.print(READ_STRING_EXPRESSION);
+
+            return Console.readLine();
+        } catch (NullPointerException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+}
