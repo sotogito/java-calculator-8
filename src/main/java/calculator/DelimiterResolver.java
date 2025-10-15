130433f01ce6420371326c343a6aeece7373cd43
sotogito <sotogitoarchive@gmail.com>
2025-10-15 17:28:36 +0900

    refactor : DelimiterMatcher 이름 변경

--- src/main/java/calculator/DelimiterResolver.java
+++ src/main/java/calculator/DelimiterResolver.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class DelimiterMatcher {
+public class DelimiterResolver {
     private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("^//(.)\\\\n(.*)$");
     private static final String ESCAPE_CHARACTER = "\\";
     private static final List<String> META_CHARACTERS = new ArrayList<>(
